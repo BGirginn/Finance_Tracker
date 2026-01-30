3919807e4479f1b4e3d6c1a98ae5093aba4661e0
BGirginn <girginbora30@gmail.com>
2026-01-30 23:27:09 +0300

    Revert "chore: remove 'Hediye' default income category"
    This reverts commit 1c226f8e008319010a03baf4957ac7cb72ed8fb5.

--- lib/domain/entities/expense_category.dart
+++ lib/domain/entities/expense_category.dart
@@ -188,11 +188,18 @@ List<ExpenseCategory> getDefaultIncomeCategories() {
       sortOrder: 2,
     ),
     ExpenseCategory(
+      name: 'Hediye',
+      iconName: 'card_giftcard',
+      colorHex: 'FF9800', // Orange
+      isDefault: true,
+      sortOrder: 3,
+    ),
+    ExpenseCategory(
       name: 'Faiz',
       iconName: 'account_balance',
       colorHex: '3F51B5', // Indigo
       isDefault: true,
-      sortOrder: 3,
+      sortOrder: 4,
     ),
   ];
 }
